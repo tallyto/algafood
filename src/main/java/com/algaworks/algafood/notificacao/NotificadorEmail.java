8573f793e80ae7af5e485ecf58ec05106d371173
tallyto <rodrigues.tallyto@hotmail.com>
2022-07-09 15:22:14 -0300

    Dependencia opcional com @Autowired

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -4,25 +4,13 @@ import com.algaworks.algafood.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 
+//@Component
 public class NotificadorEmail implements Notificador {
-    private boolean caixaAlta = false;
-    private String hostServidorSmtp;
-
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
-    }
-
-    public NotificadorEmail(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp;
-        System.out.println("Notificador email");
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        if (this.caixaAlta) {
-            mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificando %s atraves do email %s usando SMTP %s: %s\n", cliente.getNome(),
-                cliente.getEmail(), this.hostServidorSmtp, mensagem);
+
+        System.out.printf("Notificando %s atraves do email %s: %s\n", cliente.getNome(),
+                cliente.getEmail(), mensagem);
     }
 }
--- src/main/java/com/algaworks/algafood/service/AtivacaoCliente.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoCliente.java
@@ -9,16 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoCliente {
 
+    @Autowired(required = false)
     private Notificador notificador;
-
-    @Autowired // Define o construtor a ser usado
-    public AtivacaoCliente(NotificadorEmail notificador) {
-        this.notificador = notificador;
-        System.out.println("Ativacao cliente" + notificador);
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
+        if(notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
+        } else {
+            System.out.println("Nao existe notificador, mas cliente foi ativado");
+        }
     }
 }
