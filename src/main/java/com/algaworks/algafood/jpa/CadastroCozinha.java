b591e495b2ef0a4309378588069045c8f009a8f1
tallyto <rodrigues.tallyto@hotmail.com>
2022-08-10 21:49:23 -0300

    Adicionando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.jpa;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,8 +13,14 @@ import java.util.List;
 public class CadastroCozinha {
     @PersistenceContext // anotacao para injetar a dependencia do JPA
     private EntityManager manager;
+
     public List<Cozinha> listar() {
-      TypedQuery<Cozinha> query= manager.createQuery("from Cozinha",Cozinha.class);
-      return query.getResultList();
+        TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        return manager.merge(cozinha);
     }
 }
---
+++ src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class InclusaoCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApplication.class)
+                .web(WebApplicationType.NONE).run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+        cozinha1 = cadastroCozinha.adicionar(cozinha1);
+        cozinha2 = cadastroCozinha.adicionar(cozinha2);
+
+        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+
+    }
+}
