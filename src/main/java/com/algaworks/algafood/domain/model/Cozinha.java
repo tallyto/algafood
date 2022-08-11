6f46693972a7c15bc628cd7a13ea334e816db5ce
tallyto <rodrigues.tallyto@hotmail.com>
2022-08-10 21:10:46 -0300

    Impportando dados de teste com import

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,14 +1,15 @@
 package com.algaworks.algafood.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Objects;
 
 @Entity // entidade do banco de dados
 public class Cozinha {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
