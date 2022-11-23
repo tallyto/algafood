d882f1d402d7ee159fae13d1e014119e87fe46ce
tallyto <rodrigues.tallyto@hotmail.com>
2022-11-23 20:32:40 -0300

    Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,13 +1,10 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.*;
-import org.hibernate.Hibernate;
 
 import javax.persistence.*;
-import java.util.Objects;
+
 @JsonRootName("cozinha")
 @Data
 @Entity // entidade do banco de dados
@@ -16,8 +13,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonProperty(value = "titulo")
-//    @JsonIgnore
     @Column(nullable = false)
     private String nome;
 }
