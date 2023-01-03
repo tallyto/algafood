93dfd2a2263a4ca9e6d733c59d7c7b9f4d6c9fda
tallyto <rodrigues.tallyto@hotmail.com>
2023-01-03 15:59:56 -0300

    Analisando o impacto do relacionamento muitos-para-muitos na REST API

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -5,10 +5,9 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class CadastroRestauranteService {
@@ -21,16 +20,21 @@ public class CadastroRestauranteService {
     public Restaurante salvar(Restaurante restaurante) {
         Long cozinhaId = restaurante.getCozinha().getId();
         Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException(
-                        String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
+            .orElseThrow(() -> new EntidadeNaoEncontradaException(
+                String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
 
         restaurante.setCozinha(cozinha);
         return restauranteRepository.save(restaurante);
     }
 
     public Restaurante atualizar(Long restauranteId, Restaurante restaurante) {
-        restauranteRepository.findById(restauranteId).orElseThrow(
-                () -> new EntidadeNaoEncontradaException(String.format("Não existe cadastro de restaurante com código %d", restauranteId)));
-        return salvar(restaurante);
+
+        Restaurante restauranteAtual = restauranteRepository.findById(restauranteId).orElseThrow(() ->
+            new EntidadeNaoEncontradaException(
+                String.format("Não existe um cadastro de restaurante com código %d", restauranteId)));
+
+        BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento");
+
+        return salvar(restauranteAtual);
     }
 }
