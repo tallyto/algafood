5a8698687af645932f429c03ca89f4cb9ccf0241
tallyto <rodrigues.tallyto@hotmail.com>
2023-01-10 16:28:16 -0300

    Padronizando o formato de problemas no corpo de respostas com a RFC 7807

---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL) // não vai incluir campos nulos no JSON
+@Getter
+@Builder
+public class Problem {
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+}
