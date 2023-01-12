4a55fe811c25b553121a0504d80a98d79d465219
tallyto <rodrigues.tallyto@hotmail.com>
2023-01-12 13:21:19 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL) // não vai incluir campos nulos no JSON
 @Getter
@@ -16,4 +17,12 @@ public class Problem {
     private String detail;
     private String userMessage;
     private LocalDateTime timestamp;
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public  static class  Field {
+        private String name;
+        private String userMessage;
+    }
 }
