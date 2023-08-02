904a696d902dcf5b27cfc403f336e0b075de3a60
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-02 13:17:42 -0300

    desafio - tratando a exception customizada de validacoes programaticas

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.core.validation.ValidacaoException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
@@ -8,13 +9,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import javax.validation.ValidationException;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -53,45 +55,48 @@ public class RestauranteController {
     }
 
     @PatchMapping("/{restauranteId}")
-    public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+    public Restaurante atualizarParcial(@PathVariable Long restauranteId,
+                                        @RequestBody Map<String, Object> campos, HttpServletRequest request) {
         Restaurante restauranteAtual = restauranteService.buscar(restauranteId);
-        merge(campos, restauranteAtual);
+
+        merge(campos, restauranteAtual, request);
         validate(restauranteAtual, "restaurante");
-        return restauranteService.atualizarParcial(restauranteId, restauranteAtual);
+
+        return atualizar(restauranteId, restauranteAtual);
     }
 
-    private void validate(Restaurante restaurante, String objectName){
+    private void validate(Restaurante restaurante, String objectName) {
         BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(restaurante, objectName);
-        validator.validate(restaurante,bindingResult);
-        if(bindingResult.hasErrors()){
-            throw  new ValidationException();
+        validator.validate(restaurante, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidacaoException(bindingResult);
         }
     }
 
+    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino,
+                       HttpServletRequest request) {
+        ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 
-    @SuppressWarnings("deprecation") // Para evitar o warning do HttpMessageNotReadableException
-    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
         try {
-
             ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);// Falha se houver propriedades ignoradas
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true); // Falha se houver propriedades desconhecidas
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
             Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
             dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
                 Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-                assert field != null;
                 field.setAccessible(true);
+
                 Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
                 ReflectionUtils.setField(field, restauranteDestino, novoValor);
             });
-
-
         } catch (IllegalArgumentException e) {
             Throwable rootCause = ExceptionUtils.getRootCause(e);
-            throw new HttpMessageNotReadableException(e.getMessage(), rootCause);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
         }
-
     }
 
 }
