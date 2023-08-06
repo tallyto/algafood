9d60ce3c9b5af5286ec22c70e10ce4aa3bc10652
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-06 14:32:29 -0300

    Criando e rodando um teste de integracaao com Spring Boot JUnit e AssertJ

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafood;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+    @Autowired
+    CadastroCozinhaService cozinhaService;
+
+    @Test
+    public void testarCadastroCozinhaComSucesso() {
+        // cenarario
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome("Chinesa");
+        // acao
+        novaCozinha = cozinhaService.salvar(novaCozinha);
+
+        // validacao
+        assertThat(novaCozinha).isNotNull();
+        assertThat(novaCozinha.getId()).isNotNull();
+    }
+
+    @Test
+    public void deveFalharAoCadastrarCozinhaQuandoSemNome(){
+        Assertions.assertThrows(ConstraintViolationException.class, ()-> {
+            Cozinha cozinha = new Cozinha();
+            cozinha.setNome(null);
+            cozinhaService.salvar(cozinha);
+        });
+    }
+
+}
