a27502676d3e06460254f9b6378ca86201b8fee4
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-07 11:13:38 -0300

    Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/algaworks/algafood/core/validation/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/validation/jackson/JacksonMixinModule.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.core.validation.jackson;
 
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.mixin.CidadeMixin;
 import com.algaworks.algafood.domain.model.mixin.RestauranteMixin;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
@@ -10,5 +12,6 @@ public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
         setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
     }
 }
