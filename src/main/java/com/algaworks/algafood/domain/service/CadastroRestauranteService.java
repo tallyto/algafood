74c14ce3abe297448dd2a4f6470c984d9afb891e
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-09 15:12:38 -0300

    Mapeando para uma instancia destino (e não um tipo) com ModelMapper

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -6,7 +6,6 @@ import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,21 +34,6 @@ public class CadastroRestauranteService {
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage(), e);
         }
-    }
-
-    @Transactional
-    public Restaurante atualizar(Long restauranteId, Restaurante restaurante) {
-        Restaurante restauranteAtual = buscar(restauranteId);
-
-        BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco",
-            "dataCadastro", "dataAtualizacao");
-
-        return salvar(restauranteAtual);
-    }
-
-    @Transactional
-    public Restaurante atualizarParcial(Long restauranteId, Restaurante restaurante) {
-        return atualizar(restauranteId, restaurante);
     }
 }
 
