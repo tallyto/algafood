07da5d98eae8fb1b1c9d80b4eb519e3528030b34
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-09 11:36:06 -0300

    Refatorando e movendo toDTO para a classe de DTO

--- src/main/java/com/algaworks/algafood/api/model/DTO/RestauranteDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/DTO/RestauranteDTO.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.model.DTO;
 
+import com.algaworks.algafood.domain.model.Restaurante;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +13,16 @@ public class RestauranteDTO {
     private String nome;
     private BigDecimal taxaFrete;
     private CozinhaDTO cozinha;
+
+    public static RestauranteDTO toDTO(Restaurante restaurante) {
+        RestauranteDTO  restauranteDTO = new RestauranteDTO();
+        CozinhaDTO cozinhaDTO  = new CozinhaDTO();
+        restauranteDTO.setId(restaurante.getId());
+        restauranteDTO.setNome(restaurante.getNome());
+        restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
+        cozinhaDTO.setNome(restaurante.getCozinha().getNome());
+        cozinhaDTO.setId(restaurante.getCozinha().getId());
+        restauranteDTO.setCozinha(cozinhaDTO);
+        return restauranteDTO;
+    }
 }
