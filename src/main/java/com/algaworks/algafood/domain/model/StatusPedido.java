b95f5a1727a7ab07362829112fbf462b129b9587
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-25 15:39:58 -0300

    Refatorando o código de regas para transição de status de pedidos

--- src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
@@ -2,18 +2,27 @@ package com.algaworks.algafood.domain.model;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 public enum StatusPedido {
     CRIADO("Criado"),
-    CONFIRMADO("Confirmado"),
-    ENTREGUE("Entregue"),
+    CONFIRMADO("Confirmado", CRIADO),
+    ENTREGUE("Entregue", CONFIRMADO),
 
-    CANCELADO("Cancelado");
+    CANCELADO("Cancelado", CRIADO);
 
     private final String descricao;
+    private List<StatusPedido> statusAnteriores;
 
-    StatusPedido(String descricao){
+    StatusPedido(String descricao, StatusPedido... statusAnteriores){
         this.descricao = descricao;
+        this.statusAnteriores = Arrays.asList(statusAnteriores);
+    }
+
+    public boolean naoPodeAlterarPara(StatusPedido novoStatus){
+        return !novoStatus.statusAnteriores.contains(this);
     }
 
 }
