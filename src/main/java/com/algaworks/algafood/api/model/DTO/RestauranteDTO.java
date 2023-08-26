8f625ad141dda77d1e355807f0fa93b7369787f3
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-25 21:52:41 -0300

    Fazendo projeção de recursos com @JsonView do Jackson

--- src/main/java/com/algaworks/algafood/api/model/DTO/RestauranteDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/DTO/RestauranteDTO.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.model.DTO;
 
+import com.algaworks.algafood.api.model.view.RestauranteView;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +11,18 @@ import java.math.BigDecimal;
 @Setter
 public class RestauranteDTO {
 
+    @JsonView(RestauranteView.Resumo.class) // responsavel por dizer qual proprieade sera mostrada para o suario
     private Long id;
+
+    @JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})
     private String nome;
+
+    @JsonView(RestauranteView.Resumo.class)
     private BigDecimal taxaFrete;
+
+    @JsonView(RestauranteView.Resumo.class)
     private CozinhaDTO cozinha;
+
     private Boolean ativo;
     private Boolean aberto;
     private EnderecoDTO endereco;
