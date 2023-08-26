7ce0c78e2751d37772fdc7776606a135410a4c1c
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-26 18:49:04 -0300

    Implementando paginação e ordenação em recursos de coleção da API

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,9 +3,14 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.api.assembler.CozinhaModelAssembler;
 import com.algaworks.algafood.api.model.DTO.CozinhaDTO;
 import com.algaworks.algafood.api.model.input.CozinhaInput;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +30,10 @@ public class CozinhaController {
     private CozinhaModelAssembler cozinhaModelAssembler;
 
     @GetMapping
-    public List<CozinhaDTO> listar() {
-        return cozinhaModelAssembler.toCollectionDTO(cozinhaRepository.findAll());
+    public Page<CozinhaDTO> listar(@PageableDefault() Pageable pageable) {
+        Page<Cozinha> cozinhaPage = cozinhaRepository.findAll(pageable);
+        List<CozinhaDTO> cozinhaDTOs = cozinhaModelAssembler.toCollectionDTO(cozinhaPage.getContent());
+        return new PageImpl<>(cozinhaDTOs, pageable, cozinhaPage.getTotalElements());
     }
 
     @GetMapping("/{cozinhaId}")
