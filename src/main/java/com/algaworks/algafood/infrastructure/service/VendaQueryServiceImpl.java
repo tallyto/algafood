4e0d580ac63aa5c643a3a47c2a4677f40c7c4f31
tallyto <rodrigues.tallyto@hotmail.com>
2023-08-27 13:32:37 -0300

    Desafio - adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.infrastructure.service;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.model.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.service.VendaQueryService;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,6 +24,25 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         var builder = manager.getCriteriaBuilder();
         var query = builder.createQuery(VendaDiaria.class);
         var root = query.from(Pedido.class);
+        var predicates = new ArrayList<Predicate>();
+
+        if (filter.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filter.getRestauranteId()));
+        }
+
+        if (filter.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filter.getDataCriacaoInicio()));
+        }
+
+        if (filter.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filter.getDataCriacaoFim()));
+        }
+
+        predicates.add(builder.or(
+            builder.equal(root.get("status"), StatusPedido.CRIADO),
+            builder.equal(root.get("status"), StatusPedido.ENTREGUE)
+        ));
+
 
         var functionDateDataCriacao = builder.function("date", LocalDate.class, root.get("dataCriacao"));
 
@@ -31,6 +53,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         );
 
         query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(functionDateDataCriacao);
 
         return manager.createQuery(query).getResultList();
