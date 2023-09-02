e0242d4c96afcf470d466a36011a0e2b62d11a87
tallyto <rodrigues.tallyto@hotmail.com>
2023-09-02 13:33:30 -0300

    Adicionando o SDK Java da Amazon S3 no projeto e criando classe de serviço de storage

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Service
+//@Service
 public class LocalFotoStorageService implements FotoStorageService {
 
     @Autowired
