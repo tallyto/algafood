37636836ddafa2cb225a944f863e515ca451a10e
tallyto <rodrigues.tallyto@hotmail.com>
2023-09-09 22:06:04 -0300

    Implementando requisições condicionais com Deep ETags

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -3,14 +3,18 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.api.assembler.FormaPagamentoAssembler;
 import com.algaworks.algafood.api.model.DTO.FormaPagamentoDTO;
 import com.algaworks.algafood.api.model.input.FormaPagamentoInput;
+import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.service.FormaPagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import javax.validation.Valid;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -24,12 +28,29 @@ public class FormaPagamentoController {
     @Autowired
     FormaPagamentoAssembler assembler;
 
+    @Autowired
+    FormaPagamentoRepository formaPagamentoRepository;
+
     @GetMapping
-    public ResponseEntity<List<FormaPagamentoDTO>> listar() {
+    public ResponseEntity<List<FormaPagamentoDTO>> listar(ServletWebRequest request) {
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+        String eTag = "0";
+
+        OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getUltimaAtualizacao();
+
+        if(dataUltimaAtualizacao != null ){
+            eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+        }
+
+        if(request.checkNotModified(eTag)){
+            return null;
+        }
+
         var formasPagamentoModel = assembler.toCollectionDTO(cadastroFormaPagamento.listar());
 
         return ResponseEntity.ok()
             .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+            .eTag(eTag)
 //            .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
 //            .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
 //            .cacheControl(CacheControl.noCache())
