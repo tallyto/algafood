46ec5ba15dafff68d49b4bc1e32cae3e3697a5f7
tallyto <rodrigues.tallyto@hotmail.com>
2023-09-19 21:46:57 -0300

    Corrigindo documentação com substituição de Pageable

--- src/main/java/com/algaworks/algafood/core/openapi/OpenApiConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/OpenApiConfig.java
@@ -1,10 +1,12 @@
 package com.algaworks.algafood.core.openapi;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.core.openapi.model.PageableModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -42,6 +44,7 @@ public class OpenApiConfig implements WebMvcConfigurer {
             .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
             .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
             .additionalModels(typeResolver.resolve(Problem.class))
+            .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
             .apiInfo(apiInfo())
             .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
