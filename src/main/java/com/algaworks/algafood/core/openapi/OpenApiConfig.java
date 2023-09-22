7eedbf2d589431301e02fc242cf08a7a2f6a9b17
tallyto <rodrigues.tallyto@hotmail.com>
2023-09-21 23:13:05 -0300

    Ignorando tipos de parâmetros de operações na documentação

--- src/main/java/com/algaworks/algafood/core/openapi/OpenApiConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/OpenApiConfig.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -48,6 +49,7 @@ public class OpenApiConfig implements WebMvcConfigurer {
             .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
             .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
             .additionalModels(typeResolver.resolve(Problem.class))
+            .ignoredParameterTypes(ServletWebRequest.class)
             .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
             .alternateTypeRules(AlternateTypeRules.newRule(
                 typeResolver.resolve(Page.class, CozinhaDTO.class), CozinhasModelOpenApi.class
