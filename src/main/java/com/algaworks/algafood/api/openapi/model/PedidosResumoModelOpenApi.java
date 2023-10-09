98b6b3018c9dd3109ca2c712d61aefa511ce71ea
tallyto <rodrigues.tallyto@hotmail.com>
2023-10-09 09:25:02 -0300

    Desafio - descrevendo documentação de endpoint de estatísticas

---
+++ src/main/java/com/algaworks/algafood/api/openapi/model/PedidosResumoModelOpenApi.java
@@ -0,0 +1,9 @@
+package com.algaworks.algafood.api.openapi.model;
+
+import com.algaworks.algafood.api.model.DTO.PedidoResumoDTO;
+import io.swagger.annotations.ApiModel;
+
+@ApiModel("PedidosResumoModel")
+public class PedidosResumoModelOpenApi extends PagedModelOpenApi<PedidoResumoDTO> {
+
+}
