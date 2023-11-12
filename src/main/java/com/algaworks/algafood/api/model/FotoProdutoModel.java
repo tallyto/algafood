a7a4e21a92dcd4ca02fc84124cd5c43ebb156589
tallyto <rodrigues.tallyto@hotmail.com>
2023-11-12 14:28:50 -0300

    Desafio - adicionando links para recurso de foto de produto v2

--- src/main/java/com/algaworks/algafood/api/model/FotoProdutoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/FotoProdutoModel.java
@@ -1,14 +1,26 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
-@Getter
+@Relation(collectionRelation = "fotos")
 @Setter
-public class FotoProdutoModel {
+@Getter
+public class FotoProdutoModel extends RepresentationModel<FotoProdutoModel> {
+
+    @ApiModelProperty(example = "b8bbd21a-4dd3-4954-835c-3493af2ba6a0_Prime-Rib.jpg")
     private String nomeArquivo;
+
+    @ApiModelProperty(example = "Prime Rib ao ponto")
     private String descricao;
+
+    @ApiModelProperty(example = "image/jpeg")
     private String contentType;
+
+    @ApiModelProperty(example = "202912")
     private Long tamanho;
 
 }
