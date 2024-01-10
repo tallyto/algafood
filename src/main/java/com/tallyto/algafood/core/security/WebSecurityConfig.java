9e69f73dd3c0b6d2c8cc62fd2da0c0a32b7189f7
tallyto <rodrigues.tallyto@hotmail.com>
2024-01-10 13:19:31 -0300

    Configurando a validação de JWT no Resource Server com a chave pública

--- src/main/java/com/tallyto/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/tallyto/algafood/core/security/WebSecurityConfig.java
@@ -2,20 +2,13 @@ package com.tallyto.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
-@Profile("developer")
 public class WebSecurityConfig {
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,12 +20,6 @@ public class WebSecurityConfig {
                 .oauth2ResourceServer().jwt();
 
         return http.build();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        var secretKey = new SecretKeySpec("df5f8fdd4c0db669bb2d7546584a2ff08694fe29b93426ebbfcf135161029b94".getBytes(), "HmacSHA256");
-        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 
