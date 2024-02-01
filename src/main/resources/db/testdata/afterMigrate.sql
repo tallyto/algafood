7ab0f721e42811bc7486db71309632117c869b45
tallyto <rodrigues.tallyto@hotmail.com>
2024-01-31 22:34:02 -0300

    Carregando as permissões concedidas na autenticação

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -89,19 +89,24 @@ insert into cozinha (id, nome)
 values (4, 'Macuxi');
 
 -- Inserir o endereço para todos os restaurantes
-insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero,
+                         endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
 values (1, 'Restaurante Dona Maria', 9.0, 1, 1, '00000000', 'Rua Principal', 123, 'Sala 101', 'Centro', utc_timestamp, utc_timestamp);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero,
+                         endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
 values (2, 'Lá Casa de Pastel', 10.0, 2, 2, '11111111', 'Avenida da Praça', 456, NULL, 'Bairro Alegre', utc_timestamp, utc_timestamp);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero,
+                         endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
 values (3, 'Chico do Carneiro', 0, 2, 2, '22222222', 'Rua da Serra', 789, 'Casa 3', 'Montanha', utc_timestamp, utc_timestamp);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero,
+                         endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
 values (4, 'Bobs', 0, 1, 1, '33333333', 'Avenida da Liberdade', 321, NULL, 'Centro', utc_timestamp, utc_timestamp);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero,
+                         endereco_complemento, endereco_bairro, data_cadastro, data_atualizacao)
 values (5, 'Restaurante Dona Chica', 0, 1, 1, '44444444', 'Rua do Pastel', 1, 'Casa', 'Centro', utc_timestamp, utc_timestamp);
 
 insert into forma_pagamento (id, descricao, data_atualizacao)
@@ -111,43 +116,73 @@ values (2, 'dinheiro', utc_timestamp);
 insert into forma_pagamento (id, descricao, data_atualizacao)
 values (3, 'pix', utc_timestamp);
 
-insert into grupo (id, nome) values (1, 'Gerente'), (2, 'Vendedor'), (3, 'Secretária'), (4, 'Cadastrador');
+insert into grupo (id, nome)
+values (1, 'Gerente'),
+       (2, 'Vendedor'),
+       (3, 'Secretária'),
+       (4, 'Cadastrador');
 
-insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
-insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
-insert into permissao (id, nome, descricao) values (3, 'CONSULTAR_FORMAS_PAGAMENTO', 'Permite consultar formas de pagamento');
-insert into permissao (id, nome, descricao) values (4, 'EDITAR_FORMAS_PAGAMENTO', 'Permite criar ou editar formas de pagamento');
-insert into permissao (id, nome, descricao) values (5, 'CONSULTAR_CIDADES', 'Permite consultar cidades');
-insert into permissao (id, nome, descricao) values (6, 'EDITAR_CIDADES', 'Permite criar ou editar cidades');
-insert into permissao (id, nome, descricao) values (7, 'CONSULTAR_ESTADOS', 'Permite consultar estados');
-insert into permissao (id, nome, descricao) values (8, 'EDITAR_ESTADOS', 'Permite criar ou editar estados');
-insert into permissao (id, nome, descricao) values (9, 'CONSULTAR_USUARIOS', 'Permite consultar usuários');
-insert into permissao (id, nome, descricao) values (10, 'EDITAR_USUARIOS', 'Permite criar ou editar usuários');
-insert into permissao (id, nome, descricao) values (11, 'CONSULTAR_RESTAURANTES', 'Permite consultar restaurantes');
-insert into permissao (id, nome, descricao) values (12, 'EDITAR_RESTAURANTES', 'Permite criar, editar ou gerenciar restaurantes');
-insert into permissao (id, nome, descricao) values (13, 'CONSULTAR_PRODUTOS', 'Permite consultar produtos');
-insert into permissao (id, nome, descricao) values (14, 'EDITAR_PRODUTOS', 'Permite criar ou editar produtos');
-insert into permissao (id, nome, descricao) values (15, 'CONSULTAR_PEDIDOS', 'Permite consultar pedidos');
-insert into permissao (id, nome, descricao) values (16, 'GERENCIAR_PEDIDOS', 'Permite gerenciar pedidos');
-insert into permissao (id, nome, descricao) values (17, 'GERAR_RELATORIOS', 'Permite gerar relatórios');
+insert into permissao (id, nome, descricao)
+values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+insert into permissao (id, nome, descricao)
+values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+insert into permissao (id, nome, descricao)
+values (3, 'CONSULTAR_FORMAS_PAGAMENTO', 'Permite consultar formas de pagamento');
+insert into permissao (id, nome, descricao)
+values (4, 'EDITAR_FORMAS_PAGAMENTO', 'Permite criar ou editar formas de pagamento');
+insert into permissao (id, nome, descricao)
+values (5, 'CONSULTAR_CIDADES', 'Permite consultar cidades');
+insert into permissao (id, nome, descricao)
+values (6, 'EDITAR_CIDADES', 'Permite criar ou editar cidades');
+insert into permissao (id, nome, descricao)
+values (7, 'CONSULTAR_ESTADOS', 'Permite consultar estados');
+insert into permissao (id, nome, descricao)
+values (8, 'EDITAR_ESTADOS', 'Permite criar ou editar estados');
+insert into permissao (id, nome, descricao)
+values (9, 'CONSULTAR_USUARIOS', 'Permite consultar usuários');
+insert into permissao (id, nome, descricao)
+values (10, 'EDITAR_USUARIOS', 'Permite criar ou editar usuários');
+insert into permissao (id, nome, descricao)
+values (11, 'CONSULTAR_RESTAURANTES', 'Permite consultar restaurantes');
+insert into permissao (id, nome, descricao)
+values (12, 'EDITAR_RESTAURANTES', 'Permite criar, editar ou gerenciar restaurantes');
+insert into permissao (id, nome, descricao)
+values (13, 'CONSULTAR_PRODUTOS', 'Permite consultar produtos');
+insert into permissao (id, nome, descricao)
+values (14, 'EDITAR_PRODUTOS', 'Permite criar ou editar produtos');
+insert into permissao (id, nome, descricao)
+values (15, 'CONSULTAR_PEDIDOS', 'Permite consultar pedidos');
+insert into permissao (id, nome, descricao)
+values (16, 'GERENCIAR_PEDIDOS', 'Permite gerenciar pedidos');
+insert into permissao (id, nome, descricao)
+values (17, 'GERAR_RELATORIOS', 'Permite gerar relatórios');
 
 --  Adiciona todas as permissoes no grupo do gerente
 insert into grupo_permissao (grupo_id, permissao_id)
-select 1, id from permissao;
+select 1, id
+from permissao;
 
 --  Adiciona permissoes no grupo do vendedor
 insert into grupo_permissao (grupo_id, permissao_id)
-select 2, id from permissao where nome like 'CONSULTAR_%';
+select 2, id
+from permissao
+where nome like 'CONSULTAR_%';
 
-insert into grupo_permissao (grupo_id, permissao_id) values (2, 14);
+insert into grupo_permissao (grupo_id, permissao_id)
+values (2, 14);
 
 --  Adiciona permissoes no grupo do auxiliar
 insert into grupo_permissao (grupo_id, permissao_id)
-select 3, id from permissao where nome like 'CONSULTAR_%';
+select 3, id
+from permissao
+where nome like 'CONSULTAR_%';
 
 --  Adiciona permissoes no grupo cadastrador
 insert into grupo_permissao (grupo_id, permissao_id)
-select 4, id from permissao where nome like '%_RESTAURANTES' or nome like '%_PRODUTOS';
+select 4, id
+from permissao
+where nome like '%_RESTAURANTES'
+   or nome like '%_PRODUTOS';
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id)
 values (1, 1);
@@ -210,7 +245,12 @@ values (9, 'ciclano2', 'ciclano2@teste.com', '$2a$12$N4vn6FFL/7Upq7NBAVBDAedVlQY
 insert into usuario (id, nome, email, senha, data_cadastro)
 values (10, 'fulana2', 'fulana2@teste.com', '$2a$12$N4vn6FFL/7Upq7NBAVBDAedVlQYLuiI8nLB5ta365Fb.QccbzZu0e', utc_timestamp);
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+insert into usuario_grupo (usuario_id, grupo_id)
+values (1, 1),
+       (1, 2),
+       (2, 2),
+       (3, 4),
+       (4, 4);
 
 insert into restaurante_usuario_responsavel(restaurante_id, usuario_id)
 values (1, 1);
@@ -227,28 +267,38 @@ values (3, 2);
 
 -- Inserção Básica
 INSERT INTO pedido (codigo, data_criacao, subtotal, taxa_frete, valor_total, usuario_cliente_id, forma_pagamento_id,
-                    restaurante_id, status, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero)
-VALUES ('77103e21-49ef-11ee-a210-0242ac120003', '2023-08-22 10:00:00', 50.00, 5.00, 55.00, 1, 1, 1, 'CRIADO', 1, 'Bairro 1', '12345-678', 'Complemento 1', 'Rua 1', '123');
+                    restaurante_id, status, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro,
+                    endereco_numero)
+VALUES ('77103e21-49ef-11ee-a210-0242ac120003', '2023-08-22 10:00:00', 50.00, 5.00, 55.00, 1, 1, 1, 'CRIADO', 1, 'Bairro 1', '12345-678',
+        'Complemento 1', 'Rua 1', '123');
 
 -- Inserção com Status e Data de Entrega
 INSERT INTO pedido (codigo, data_criacao, data_entrega, status, subtotal, taxa_frete, valor_total, usuario_cliente_id,
-                    forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero)
-VALUES ('1e0bcff5-7081-11ee-82b5-0242ac120002', '2023-08-03 11:30:00', '2023-08-23 18:00:00', 'CRIADO', 75.00, 8.00, 83.00, 2, 2, 2, 2, 'Bairro 2', '54321-876', 'Complemento 2', 'Rua 2', '456');
+                    forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento,
+                    endereco_logradouro, endereco_numero)
+VALUES ('1e0bcff5-7081-11ee-82b5-0242ac120002', '2023-08-03 11:30:00', '2023-08-23 18:00:00', 'CRIADO', 75.00, 8.00, 83.00, 2, 2, 2, 2,
+        'Bairro 2', '54321-876', 'Complemento 2', 'Rua 2', '456');
 
 -- Inserção com Cancelamento
 INSERT INTO pedido (codigo, data_criacao, data_cancelamento, status, subtotal, taxa_frete, valor_total,
-                    usuario_cliente_id, forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero)
-VALUES ('1e0bfb78-7081-11ee-82b5-0242ac120002', '2023-08-12 15:45:00', '2023-08-22 16:30:00', 'CRIADO', 30.00, 3.50, 33.50, 3, 1, 3, 3, 'Bairro 3', '98765-432', 'Complemento 3', 'Rua 3', '789');
+                    usuario_cliente_id, forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep,
+                    endereco_complemento, endereco_logradouro, endereco_numero)
+VALUES ('1e0bfb78-7081-11ee-82b5-0242ac120002', '2023-08-12 15:45:00', '2023-08-22 16:30:00', 'CRIADO', 30.00, 3.50, 33.50, 3, 1, 3, 3,
+        'Bairro 3', '98765-432', 'Complemento 3', 'Rua 3', '789');
 
 -- Inserção com Confirmação
 INSERT INTO pedido (codigo, data_criacao, data_confirmacao, status, subtotal, taxa_frete, valor_total,
-                    usuario_cliente_id, forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero)
-VALUES ('1e0c2538-7081-11ee-82b5-0242ac120002', '2023-08-26 09:15:00', '2023-08-22 10:00:00', 'CRIADO', 60.00, 6.00, 66.00, 4, 3, 4, 2, 'Bairro 4', '54321-987', 'Complemento 4', 'Rua 4', '987');
+                    usuario_cliente_id, forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep,
+                    endereco_complemento, endereco_logradouro, endereco_numero)
+VALUES ('1e0c2538-7081-11ee-82b5-0242ac120002', '2023-08-26 09:15:00', '2023-08-22 10:00:00', 'CRIADO', 60.00, 6.00, 66.00, 4, 3, 4, 2,
+        'Bairro 4', '54321-987', 'Complemento 4', 'Rua 4', '987');
 
 -- Inserção Completa
 INSERT INTO pedido (codigo, data_criacao, data_confirmacao, data_entrega, data_cancelamento, status, subtotal,
-                    taxa_frete, valor_total, usuario_cliente_id, forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero)
-VALUES ('1e0c5337-7081-11ee-82b5-0242ac120002', '2023-08-18 14:20:00', '2023-08-22 15:00:00', '2023-08-23 12:30:00', NULL, 'CRIADO', 90.00, 10.00,
+                    taxa_frete, valor_total, usuario_cliente_id, forma_pagamento_id, restaurante_id, endereco_cidade_id, endereco_bairro,
+                    endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero)
+VALUES ('1e0c5337-7081-11ee-82b5-0242ac120002', '2023-08-18 14:20:00', '2023-08-22 15:00:00', '2023-08-23 12:30:00', NULL, 'CRIADO', 90.00,
+        10.00,
         100.00, 5, 3, 4, 3, 'Bairro 5', '12345-789', 'Complemento 5', 'Rua 5', '555');
 
 -- Pedido 1
